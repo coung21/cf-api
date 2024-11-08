7dbac4f166e93026346657b9ac6b9fbc7cf03a31
Stork Nguyen <storknguyen.286@gmail.com>
2024-11-09 00:37:56 +0700

    add disease data and update prediction route to return detailed results

--- routes/predictor.py
+++ routes/predictor.py
@@ -6,8 +6,9 @@ from model.sam import sam_preprocess
 from model.model import predict
 from cloudinary import uploader
 from services.history_service import add_history
-
+import json as jsn
 predictor_routes = Blueprint("predictor", url_prefix="/predictor")
+
 
 @predictor_routes.route("/predict", methods=["POST"])
 async def predict_route(request):
@@ -17,31 +18,34 @@ async def predict_route(request):
     if file is None:
         return json({"error": "No file is attached"}, status=400)
 
-  
     image = read_file_as_image(file.body)
 
-   
     if not isinstance(image, torch.Tensor):
         return json({"error": "Failed to process image to Tensor"}, status=500)
 
- 
     image = image.unsqueeze(0)
-
 
     image = sam_preprocess(image.cpu().numpy())
 
     result = await predict(image)
-    
+
     cloudinary_response = uploader.upload(file.body)
-    
+
     history_data = {
         "user_id": user_id,
         "image_url": cloudinary_response["secure_url"],
         "result": result.tolist()[0],
     }
-    
+
     error = await add_history(request.app.ctx.db, history_data)
     if error:
         return json({"error": f"Failed to save history: {error}"}, status=400)
-    
-    return json({"result": result.tolist()[0], "image_url": cloudinary_response["secure_url"]})
+    # Load data from data.json
+    with open('data.json', 'r', encoding='utf-8') as file:
+        pred = jsn.load(file)  # Changed 'data' to 'pred'
+
+    # Find the object with matching idx
+    matching_obj = next(
+        (item for item in pred if item['idx'] == result.tolist()[0]), None)
+    # Display the object if found
+    return json({"result": matching_obj, "image_url": cloudinary_response["secure_url"]})
