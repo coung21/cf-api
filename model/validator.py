f29009c7f5d11456854a21520ec817267b2f7d8b
coungdev <coungdev@gmail.com>
2024-11-25 10:09:35 +0700

    refactor: update model validator

--- model/validator.py
+++ model/validator.py
@@ -10,29 +10,29 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 class Validator:
     def __init__(self, device):
         self.model = mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
-        self.model.classifier[1] = nn.Sequential(
-            nn.Linear(self.model.last_channel, 1),
-            nn.Sigmoid()
-        )
-        self.model.load_state_dict(torch.load('model/checkpoints/mobilenetv2_leaf_classifier.pth', map_location=device))
+        # self.model = mobilenet_v2(pretrained=True)
+        self.model.classifier[1] = nn.Linear(self.model.last_channel, 1)
+        self.model.load_state_dict(torch.load('model/checkpoints/leaf_disease_model_mobilenetv2.pth', map_location=device))
         self.model = self.model.to(device)
         
         self.transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+    transforms.RandomResizedCrop((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
         
     def predict(self, image):
             image = self.transform(image).to(device)
             image = image.unsqueeze(0)
             
             with torch.no_grad():
-                self.model.eval()
-                output = self.model(image)
-                prob = output.argmax(dim=1)
-                prob = prob.item()
-                return prob
+                output = self.model(image).squeeze()
+                probability = torch.sigmoid(output).item()
+                pred = 1 if probability > 0.5 else 0
+                print(pred)
+                return pred
     
     def read_file(self, data):
         image = Image.open(BytesIO(data)).convert("RGB")
